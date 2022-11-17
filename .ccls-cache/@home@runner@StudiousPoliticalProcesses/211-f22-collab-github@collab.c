fb7944f12cd614dce2cdcd07a8da8418337a2e09
Srijita Banerjee <76555991+SrijitaBanerjee2002@users.noreply.github.com>
2022-11-17 14:45:05 +0000

    Added function: jpate308_sbane4(), Edited main: accepted an integer input from the user and called function jpate308_sbane4() Commented out function reed_ekidane2() in main.

--- .ccls-cache/@home@runner@StudiousPoliticalProcesses/211-f22-collab-github@collab.c
+++ .ccls-cache/@home@runner@StudiousPoliticalProcesses/211-f22-collab-github@collab.c
@@ -20,17 +20,33 @@ void reed_ekidan2() {
   printf("Dale Reed and Ellen Kidane\n");
   printf("We learned that git and GitHub are amazing, powerful tools!\n");
 }
-void jpate308_sbane4(){
+void jpate308_sbane4(int *choice){
   printf("Jash Patel and Srijita Banerjee\n");
   printf("We have learnt some basic commands which are used across GIThub and also github is best used for version control of your program\n");
+
+  int num1=0, num2=0;
+  printf("Enter 2 integers\n");
+  scanf("%i",&num1);
+  scanf("%i",&num2);
+
+  switch(*choice) {
+    case 1: printf("Sum = %i\n",(num1+num2)); break;
+    case 2: printf("Difference = %i\n",(num1-num2)); break;
+    case 3: printf("Product = %i\n",(num1*num2)); break;
+    case 4: printf("Division = %i\n",(num1/num2)); break;
+    default: printf("Invalid choice\n"); break;
+  }
 }
 
 int main()
 {
-    printf("This code has been created by the students of CS 211, Fall 2022!\n\n");
+    printf("This code has been created by Srijita Banerjee and Jash Patel of CS 211, Fall 2022!\n\n");
     
     //reed_ekidan2();
-    jpate308_sbane4(); 
+    int choice=0;
+    printf("Enter your choice 1-4!\n");
+    scanf("%i",&choice);
+    jpate308_sbane4(&choice); 
     
     return 0;
 }
